6ca498726f6902a85596c0322f8cd5f406128539
Rocky <rahul.annem@gmail.com>
2024-07-29 13:53:16 -0500

    Changes Made

--- Random_Coding/Numbering_Lists.py
+++ Random_Coding/Numbering_Lists.py
@@ -1,1 +1,28 @@
-print("This is a test")
+userlist = list()
+finallist = list()
+listnumber = 0
+appendnumber = 1
+def main():
+    while True:
+        asklist = input("Enter a word to add to the list. When done, type in 'Finished' - ")
+        if asklist == "Finished":
+            numbering()
+            print(finallist)
+            quit()
+        else:
+            userlist.append(asklist)
+def numbering():
+    for lists in userlist:
+        global listnumber
+        global appendnumber
+        lists = userlist[listnumber]
+        listnumber = str(listnumber)
+        appendnumber = str(appendnumber)
+        finished_single = lists+"_"+appendnumber
+        finallist.append(finished_single)
+        listnumber = int(listnumber)
+        appendnumber = int(appendnumber)
+        listnumber = listnumber + 1
+        appendnumber = appendnumber + 1
+
+main()
