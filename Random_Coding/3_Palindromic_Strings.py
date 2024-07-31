6738d9d5804a984abbc94ee1a2feee1c6912cbf5
Rocky <rahul.annem@gmail.com>
2024-07-31 14:16:33 -0500

    Changes Made

--- Random_Coding/3_Palindromic_Strings.py
+++ Random_Coding/3_Palindromic_Strings.py
@@ -5,8 +5,13 @@ def maincode():
     reversestring = ''.join(reversed(userstring))
     if userstring == "":
         print("Try typing in something next time.")
+        userstring = "(no string entered)"
+        reversestring = "(no string entered)"
+
     elif reversestring == userstring:
         print("Yay, you entered a palindrome!")  
     else:
         print("No palindrome! Try again!")
+    print("The original version of the string was " + userstring)
+    print("The reversed version of the string is " + reversestring)
 maincode()
