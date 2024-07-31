eec6501c4821bd469d31f913ef21dae15df1cc01
Rocky <rahul.annem@gmail.com>
2024-07-31 14:06:38 -0500

    Changes Made

--- Random_Coding/Lesson_3.py
+++ Random_Coding/Lesson_3.py
@@ -1,1 +1,12 @@
-print("This is a test")
+def maincode():
+    userstring = input("Enter a palindrome. A palindrome is a word or "+ \
+                       "number that can be read the same both ways: ")
+    userstring = userstring.lower()
+    reversestring = ''.join(reversed(userstring))
+    if userstring == "":
+        print("Try typing in something next time.")
+    elif reversestring == userstring:
+        print("Yay, you entered a palindrome!")  
+    else:
+        print("No palindrome! Try again!")
+maincode()
